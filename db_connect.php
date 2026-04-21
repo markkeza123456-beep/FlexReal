f73349eed1b93bec8bba7ca33a4f5050f99fc7d1
markkeza123456-beep <markkeza123456@gmail.com>
2026-04-21 12:31:41 +0700

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,19 @@
+<?php
+$host = "aws-1-ap-northeast-1.pooler.supabase.com";
+$db   = "postgres";
+$user = "postgres.gwunrmptlmfpvidrxwdf";
+$pass = "lerninghub@";
+$port = "5432";
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+
+try {
+    $conn = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    $conn->exec("SET client_encoding TO 'UTF8'");
+    
+} catch (PDOException $e) {
+    die("การเชื่อมต่อ Supabase ล้มเหลว: " . $e->getMessage());
+}
+?>
